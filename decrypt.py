ab81913a5276c32726ba000b36421cd616df213f
Yaracoding <140946710+Yaracoding@users.noreply.github.com>
2023-08-15 17:20:41 +0300

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,10 @@
+letters =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+message= input("What is the message you would like to resotore?").lower()
+encr = ""
+k = 3
+for l in message:
+    location = letters.index(l)
+    print(location)
+    encr = encr + letters[location-k]
+
+print(encr)
